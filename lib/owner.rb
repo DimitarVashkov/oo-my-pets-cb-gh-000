f523dd86486ce635ad520b875737fe204ad2762d
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-03-27 19:26:36 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -53,7 +53,7 @@ class Owner
   end
 
   def list_pets
-    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and 1 cat(s)."
+    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
   end
 
 end
