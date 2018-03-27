99ea63f34896a44b3531325cd53388eaf5b702b0
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-03-27 19:26:58 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -52,6 +52,11 @@ class Owner
     @pets[:fishes].collect {|x| x.mood = 'happy'}
   end
 
+  def sell_pets
+    
+  end
+  
+
   def list_pets
     "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
   end
